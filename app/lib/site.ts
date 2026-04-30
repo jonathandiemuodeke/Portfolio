ad10e47ef6d1f3e590aa888335bed40f62af69ce
Jonathan <jonalearnemail@gmail.com>
2026-04-30 17:48:57 +0300

    Update default SITE_URL to new Netlify address

--- app/lib/site.ts
+++ app/lib/site.ts
@@ -1,2 +1,2 @@
-export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://jb-tech.netlify.app";
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://ictservice.netlify.app";
 
